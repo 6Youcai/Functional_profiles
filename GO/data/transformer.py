cbdd9b0ae3a6bee8343cce95550f18422bce573d
Youcai-at-Yuce <904731544@qq.com>
2017-02-28 14:42:09 +0800

    fixed cycle border

--- GO/data/transformer.py
+++ GO/data/transformer.py
@@ -2,17 +2,20 @@ import re
 
 # {ID:[], name:"", namespace:""}
 def print_dict(one_dict):
+    if "ID" not in one_dict:
+        return 0
     for one_id in one_dict["ID"]:
         arr = [one_id, one_dict["name"], one_dict["namespace"]]
         print("\t".join(arr))
+
 with open('go-basic.obo') as f:
     tmp_dict = {}
     for line in f:
-        if re.search(r'^\[Term\]', line):
-            if tmp_dict:
+        if re.search(r'^\[Term|Typedef\]', line): # ensure the last go id dict correctly and be the end
+            if "ID" in tmp_dict: # the last few dict has not the "id" key
                 print_dict(tmp_dict)
                 tmp_dict = {} # clear
-        elif re.search(r'^id', line):
+        elif re.search(r'^id:\sGO:\d+', line): # go id
             Id = re.search(r'^id:\s(.+)', line).group(1)
             tmp_dict["ID"] = [Id]
         elif re.search(r'^alt_id', line):
@@ -24,3 +27,5 @@ with open('go-basic.obo') as f:
         elif re.search(r'^namespace', line):
             namespace = re.search(r'^namespace:\s(.+)', line).group(1)
             tmp_dict["namespace"] = namespace
+    # useless here, but......
+    print_dict(tmp_dict)
