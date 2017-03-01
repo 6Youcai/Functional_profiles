ecdd5d76522cd12433c249b87f801eb4ba0cd98f
Youcai-at-Yuce <904731544@qq.com>
2017-03-01 20:01:00 +0800

    achive some function, time to back home

--- DO/enrich-do.R
+++ DO/enrich-do.R
@@ -28,17 +28,44 @@ argv <- commandArgs(TRUE)
 DEG <- argv[1]
 out_name <- argv[2]
 
-genelist <- read.table(DEG, sep = '\t', header=T)$Gene
-gene_enterzid <- bitr(genelist,
+genelist <- read.table(DEG, sep = '\t', header=T)
+gene_enterzid <- bitr(genelist$Gene,
                       fromType = "SYMBOL",
                       toType   = "ENTREZID",
                       OrgDb    = "org.Hs.eg.db")$ENTREZID
+#
 disease <- enrichDO(gene          = gene_enterzid,
                     ont           = "DO",
                     pvalueCutoff  = 1,
                     pAdjustMethod = "BH",
                     qvalueCutoff  = 1,
                     readable      = TRUE)
+
+barplot(disease, showCategory = 20)
+dotplot(disease, showCategory = 20)
+log_fc <- genelist$logFC
+names(log_fc) <- gene_enterzid
+cnetplot(disease, 
+         categorySize="pvalue",
+         foldChange = log_fc,
+         fixed = TRUE)
 disease <- as.data.frame(disease)
 disease$geneID <- str_replace_all(disease$geneID, "/", ";")
-save_table(out_dat   = disease, file_name = paste0(out_name, ".unfilter.disease.xls"))
+save_table(disease, paste0(out_name, ".unfilter.disease.xls"))
+#
+ncg <- enrichNCG(gene = gene_enterzid,
+                 pvalueCutoff = 1,
+                 pAdjustMethod = "BH",
+                 qvalueCutoff = 1,
+                 readable = TRUE)
+ncg <- as.data.frame(ncg)[, -1]
+ncg$geneID <- str_replace_all(ncg$geneID, "/", ";")
+save_table(ncg, paste0(out_name, ".unfilter.cancer.xls"))
+# 
+# 
+# enrichDGN 
+# enrichDGNv 
+# ChIPseeker 
+
+# upsetplot(disease)
+# enrichMap(disease)
